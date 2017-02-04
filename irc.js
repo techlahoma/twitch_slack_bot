2ef3c2da24545cf14d3d94d64141ad687ed4e035
dashcraft <ashcraftbookkeeping@gmail.com>
2017-02-03 21:12:24 -0600

    Added process ndoe functions

--- irc.js
+++ irc.js
@@ -24,8 +24,7 @@ let bidi = new Bidi({
 process.on('SIGTERM', () => {
   console.log('Received SIGINT.  Press Control-D to exit.');
   process.nextTick(() => process.exit());  	
-  }
-});
+  });
 
 process.on('exit', (code) => {
   console.error("Process is about to exit with code #{code}")
